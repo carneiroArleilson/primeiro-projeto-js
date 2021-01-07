5d35642d6b052b94a9e48b018376d85a8a5aa62f
carneiroArleilson <carneiroarleilson@gmail.com>
2021-01-07 18:48:54 -0400

    final aula 9

---
+++ catalogo.js
@@ -0,0 +1,37 @@
+exercício 03:
+    s.lenght //(quantos caracteres a string tem)
+    s.toUpperCase //(tudo para 'MAIÚSCULA')
+    s.toLowerCase //(tudo para 'minúscula')
+exercício 04:
+    var = n1
+    n1.toFixed(x), //"acrescenta x casas decimais no float"
+    n1.toFixed(x).replace(".",","), //"replace troca algo por outro ex. '.' por ',' "
+    n1.toLocaleString('pt-BR', {style: 'currency', currency:'BRL'})//escolhe uma moeda no caso o Real
+    n1.toLocaleString('pt-BR', {style: 'currency', currency:'USD'})//escolhe uma moeda no caso o Dolar
+operadores de atribuição
+    5 + 2 //soma =7
+    5 - 2 //subtração =3
+    5 * 2 //multiplicação =10
+    5 / 2 //divisão =2.5
+    5 % 2 //resto da divisão =1
+    5 ** 2//potenciação =25
+    x = 2 //recebe, x recebe o valor de 2
+operadores relacionais
+    5>2  //maior que =true
+    7<4  //menor que =false
+    8>=8 //maior ou igual que =true
+    9<=7 //menor ou igual que =false
+    5==5 //ver se é igual =true
+    4!=4 // ver se é diferente =false
+    5===5//operador de identidade identicos(valor e tipo) = false
+operadores LOGICOS
+    !   //negação "não"
+    &&  //conjução "e"
+    ||  //disjunção "ou"
+    media>=x? "aprovado" : "reprovado" /* compara uma operação mostra um 
+                                       resultado pra verdadeiro se verdadeiro e 
+                                       um pra falso sefalso*/
+    res = x % 2 == 0 ? 'par' : 'impar' /* res recebe par se o resto da divisão entre x e 2 for 0
+                                       e res recebe impar se o resto da divisão entre x e 2 for 
+                                       diferente de 1*/
+    
--- catálogo.js
+++
@@ -1,9 +0,0 @@
-exercício 03:
-    s.lenght (quantos caracteres a string tem)
-    s.toUpperCase (tudo para 'MAIÚSCULA')
-    s.toLowerCase (tudo para 'minúscula')
-exercício 04:
-    var = n1
-    n1.toFixed(x), "acrescenta x casas decimais no float"
-    n1.toFixed(x).replace(".",","), "replace troca algo por outro ex. '.' por ',' "
-    
